bfcfb6d9d44528c39b538dbe7e40a889d6dacc4b
kaito1111 <kaito13767@gmail.com>
2019-10-08 16:21:42 +0900

    腕とゲーム画面追加！

---
+++ GameTemplate/Game/GameSence.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "GameSence.h"
+
+
+GameSence::GameSence()
+{
+}
+
+GameSence::~GameSence()
+{
+}
+
+bool GameSence::Start()
+{
+	return true;
+}
+
+void GameSence::Update()
+{
+}
