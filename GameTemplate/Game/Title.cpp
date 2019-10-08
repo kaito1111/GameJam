f4cb6edb2442732245121d698912bca4ee481e57
kamiguchireo <kbc19b07@stu.kawahara.ac.jp>
2019-10-08 17:53:23 +0900

    仮

--- GameTemplate/Game/Title.cpp
+++ GameTemplate/Game/Title.cpp
@@ -13,16 +13,10 @@ Title::~Title()
 }
 bool::Title::Start()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
 	//インスタンス
->>>>>>> bcead7dfc736d1bfcb895de0c8d11c7ddbae7f91
 	m_spriteRender = NewGO<prefab::CSpriteRender>(0);
 	//タイトルの読み込み
 	m_spriteRender->Init(L"sprite/GGJ_title.dds", 1280,720,0);
->>>>>>> c3538f83b0fe02d377ab840b9941d9312304d0df
 	return true;
 }
 
